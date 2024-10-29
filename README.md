0ebba85c2b51ec6ca5a00e4999a356db46b89d5a
MarceloKobayashi <marcelohk2004@gmail.com>
2024-10-29 10:48:41 -0300

    1029 - Rotas do app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ponto-eletronico-backend
+
+npm init : description e author
+npm .\index.js
+npm install express
+
+gitignore
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Ola, mundo!");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//ROTAS: app.[Method]([Path], [Handler])
+app.get('/', (req, res) => {
+    res.send("Chamada ao recurso usando o get realizada com sucesso.");
+});
+
+//Retornar todos os usuários
+app.get('/users', (req, res) => {
+    res.send("Aqui vou retornar todos os usuários do sistema.");
+});
+
+//Retornar o usuário com um id específico
+app.get('/user/:id', (req, res) => {
+    console.log(req.params.id); //Valor do parâmetro 'id'
+});
+
+app.post('/rotapost', (req, res) => {
+    res.send("Chamada ao recurso usando post realizada com sucesso.");
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor aguardando requisições.");
+});
